40695984e241be0bfc075d9c7bdf1e4ddce158a9
Andros <wong22@hotmail.co.uk>
2017-10-31 15:42:42 -0700

    Final touchups

--- reactApp/components/EditorPage.js
+++ reactApp/components/EditorPage.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import MyEditor from './MyEditor';
 import {Link} from 'react-router-dom';
+import {Icon} from 'react-materialize';
 
 class EditorPage extends React.Component {
   constructor(props) {
@@ -10,7 +11,7 @@ class EditorPage extends React.Component {
   render() {
     return (
       <div className="container" style={{backgroundColor: '#2bbbad', paddingTop: '20px', paddingLeft: '40px', position:'absolute', width: '100%', height: '100%', top: '0', left: '0'}}>
-        <Link style={{color: 'white', marginBottom: '15px'}} to="/"> Back </Link>
+        <Link style={{color: 'white', marginBottom: '15px'}} to="/"><Icon>arrow_back</Icon></Link>
         <MyEditor />
       </div>
     );
