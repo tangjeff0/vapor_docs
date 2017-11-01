b244978ad38b0c4b1d780189db7be8b6aeb23049
jeff <tangj1122@gmail.com>
2017-11-01 11:28:39 -0700

    doc pw validation

--- backend/routes.js
+++ backend/routes.js
@@ -22,27 +22,41 @@ module.exports = (passport) => {
 
   router.get('/docs', (req, res) => {
     Doc.find({collaborators: req.user.id}).sort({last_edited: -1})
-      .then(resp => {
-        res.json({docs: resp});
-      })
-      .catch(err => {
-        res.json({error_message: err});
-      });
+    .then(resp => {
+      res.json({docs: resp});
+    })
+    .catch(err => {
+      res.json({error_message: err});
+    });
+  });
+
+  router.post('/checkDocPassword', (req, res) => {
+    Doc.findById(req.body.docId)
+    .then(doc => {
+      if (req.body.docPassword === doc.password) {
+        res.json({wasCorrectPassword: true});
+      } else {
+        res.status(401).json({wasCorrectPassword: false, err: 'wrong password bruv'});
+      }
+    })
+    .catch(err => {
+      res.json({err});
+    })
   });
 
   router.get('/doc/:docId', (req, res) => {
     Doc.findById(req.params.docId)
-      .then(resp => {
-        if (resp.collaborators.indexOf(req.user.id) === -1) {
-          res.json({message: 'sorry, you are not yet a collaborator on this doc :('});
-        }
-        else {
-          res.json({doc: resp});
-        }
-      })
-      .catch(err => {
-        res.json({error_message: err});
-      });
+    .then(resp => {
+      if (resp.collaborators.indexOf(req.user.id) === -1) {
+        res.json({message: 'sorry, you are not yet a collaborator on this doc :('});
+      }
+      else {
+        res.json({doc: resp});
+      }
+    })
+    .catch(err => {
+      res.json({error_message: err});
+    });
   });
 
   router.post('/doc/new', (req, res) => {
@@ -62,20 +76,19 @@ module.exports = (passport) => {
 
   router.put('/doc/:id', (req, res) => {
     Doc.findById(req.params.id)
-      .then(doc => {
-        doc.title =  req.body.title || doc.title;
-        doc.contents = req.body.contents;
-        doc.last_edit = new Date().getTime();
-        doc.revision_history = [...doc.revision_history, req.body.contents];
-        doc.save(err => {
-          if (!err) res.json({doc});
-        });
-      })
-      .catch(err => {
-        res.json({error_message: err});
+    .then(doc => {
+      doc.title =  req.body.title || doc.title;
+      doc.contents = req.body.contents;
+      doc.last_edit = new Date().getTime();
+      doc.revision_history = [...doc.revision_history, req.body.contents];
+      doc.save(err => {
+        if (!err) res.json({doc});
       });
+    })
+    .catch(err => {
+      res.json({error_message: err});
+    });
   });
-
 
   return router;
 };
