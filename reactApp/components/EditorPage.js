e8c5376098d4a2c1c9b735ffba838c2e5b8d7bf4
jeff <tangj1122@gmail.com>
2017-11-03 13:10:36 -0700

    v1 styling complete ~vaporwave

--- reactApp/components/EditorPage.js
+++ reactApp/components/EditorPage.js
@@ -20,8 +20,8 @@ class EditorPage extends React.Component {
   }
   render() {
     return (
-      <div className="container" style={{backgroundColor: '#2bbbad', paddingTop: '20px', paddingLeft: '40px', position:'absolute', width: '100%', height: '100%', top: '0', left: '0'}}>
-        <Link style={{color: 'white', marginBottom: '15px'}} to="/"><Icon>arrow_back</Icon></Link>
+      <div className="container EditorPage" >
+        <Link style={{color: 'white', marginBottom: '15px'}} to="/"><Icon className='blue-arrow'>arrow_back</Icon></Link>
         <MyEditor docId={this.props.docId} socket={this.state.socket}/>
       </div>
     );
--- webpack.config.js
+++ webpack.config.js
@@ -17,16 +17,6 @@ module.exports = {
             presets: ['react', 'es2015']
           }
         }
-      },
-      {
-        test: /\.scss$/,
-        use: [{
-          loader: "style-loader" // creates style nodes from JS strings
-        }, {
-          loader: "css-loader" // translates CSS into CommonJS
-        }, {
-          loader: "sass-loader" // compiles Sass to CSS
-        }]
       }
     ]
   },
